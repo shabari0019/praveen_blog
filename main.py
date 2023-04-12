19915c8c5cff813948243d52ad06545cab3483d7
shabari <shabari.0019@gmail.com>
2023-04-12 21:35:15 +0530

    option delete

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "RKAUkOTzTdTyKIxCUwLg0hyfMLO2FxSi"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
